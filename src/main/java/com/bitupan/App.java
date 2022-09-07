7860f16b61d20c91c9d54f4944e9ecddc9120583
bitupan-arandhara <bitupan.arandhara@swiggy.in>
2022-09-07 17:19:22 +0530

    Get item implemented

--- src/main/java/com/bitupan/App.java
+++ src/main/java/com/bitupan/App.java
@@ -1,17 +1,21 @@
 package com.bitupan;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Attribute;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.bitupan.EnvVars.EnvVars;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App 
 {
@@ -28,12 +32,15 @@ public class App
         
 
         //1. Putting An Item
+        // HashMap<String, String> customer = new HashMap<String, String>();
+        // customer.put("customerId", "c3");
+        // customer.put("customerName", "bitu");
+        // putItem(table, "t6", "20-12-2020", "PURCHASE", 15, customer);
+    
+        //2. Get Item
+        getItem(table, "t6", "20-12-2020");
 
-        HashMap<String, String> customer = new HashMap<String, String>();
-        customer.put("customerId", "c3");
-        customer.put("customerName", "bitu");
-
-        putItem(table, "t6", "20-12-2020", "PURCHASE", 15, customer);
+    
     }
     public static void putItem(Table table, String hashKeyValueString, String rangeKeyValueString, String type, int amount, HashMap<String, String> customer){
         //build item
@@ -44,6 +51,54 @@ public class App
             .withMap("customer", customer);
         //put item to the table
         PutItemOutcome outcome = table.putItem(item);
+    }
+    public static void getItem(Table table, String hashKeyValueString, String rangeKeyValueString){
+        Item item = table.getItem("transactionId", hashKeyValueString, "date", rangeKeyValueString);
+        //LETS PLAY WITH item
 
+        //1. No of attributes
+        //int  noOfAttr = item.numberOfAttributes();
+        //System.out.println("Number of attributes: "+ noOfAttr);
+        
+        //2. get an attribute named "customer"
+        //System.out.println(item.get("customer"));
+
+        //3. convert the item to a map
+        //Map<String, Object> map = item.asMap();
+        //System.out.println(map);
+        //System.out.println(map.get("customer"));
+
+        //4. Get an attribute named "customer" as map
+        //Map<String, Object> customerAsMap = item.getMap("customer");
+        //System.out.println(customerAsMap.get("customerName"));
+
+        //5. Get an attribute named "customer" as JSON string
+        //System.out.println(item.getJSON("customer"));
+
+        //6. get item as an Iterator 
+        //System.out.println(item.attributes());
+        // for (Map.Entry<String, Object> attribute : item.attributes()) {
+        //     System.out.println(attribute);
+        // }
+
+        //7. Convert the item to a JSON String
+        //String itemAsJsonString = item.toJSONPretty();
+        //System.out.println(itemAsJsonString);
+        // ObjectMapper mapper = new ObjectMapper();
+        // try{
+        //     JsonNode rootNode = mapper.readTree(itemAsJsonString);
+        //     //System.out.println(rootNode);
+        //     //rootNode.path("date");
+        //     //System.out.println(rootNode.path("customer").path("customerName"));
+        //     JsonNode customerAJsonNode = rootNode.path("customer");
+        //     System.out.println(customerAJsonNode); 
+        //     if(Integer.parseInt(rootNode.path("amount").toString())==15){
+        //         System.out.println("true");
+        //     }
+        //     else System.out.println("false");
+        // }
+        // catch(Exception e){
+        //     System.out.println(e);
+        // }
     }
 }
